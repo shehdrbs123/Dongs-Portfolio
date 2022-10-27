e9eb97d3d6c457d6a479d8f4a337cfe4a5ebecbf
shehdrbs123 <shehdrbs123@naver.com>
2022-10-27 10:14:17 +0900

    Update CommonButtonScript.cs

--- UnityProject/NetworkShooting/Description/UI, Scene/Connect Scene/_Scripts/CommonButtonScript.cs
+++ UnityProject/NetworkShooting/Description/UI, Scene/Connect Scene/_Scripts/CommonButtonScript.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CommonButtonScript : MonoBehaviour {
-
-
-    void Start()
-    {
-        //Screen.SetResolution(1440, 2560, false);
-        //Screen.SetResolution(Screen.height * 9/16, Screen.height, false);
-    }
-
     public void switchScene(string name){
-		SceneManager.LoadScene (name);
+        SceneManager.LoadScene (name);
     }
 
-	public void exitGame(){
-		Application.Quit ();
+    public void exitGame(){
+        Application.Quit ();
     }
 }
